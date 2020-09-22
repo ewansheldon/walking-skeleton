e21624677e5d429680fe2724bede33980bdab2c4
ewansheldon <ewansheldon@gmail.com>
2020-09-22 14:03:38 +0100

    creates basic spark api with tests for walking skeleton

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import spark.Spark;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
